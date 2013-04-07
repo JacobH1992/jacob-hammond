6c66002dad5a2894d3b273db92a68ecfae77bc9f
JHammond <ss_clan_cod@hotmail.com>
2013-04-07 15:18:49 +0100

    Removed loop from image-orientation.php

--- php/image-orientation.php
+++ php/image-orientation.php
@@ -3,32 +3,27 @@
 
 function getOrientation( $imageSrc ) {
 
-$imageSrc = getElementsByTagName('img');
 
-    foreach ($imageSrc as $image) {
-
-        // get the widths of each image
-        $width = $image->imagesx;
-
-        $height = $image->imagesy;
+    // get the widths of each image
+    $width = $imageSrc->imagesx;
+    $height = $imageSrc->imagesy;
 
 
-        // the existing classes already on the images
-        $existing_classes = $image->getAttribute('class');
+    // the existing classes already on the images
+    $existing_classes = $imageSrc->getAttribute('class');
 
-        // the class we're adding
-        $landscape_class = 'landscape';
-    	$portrait_class = 'portrait';
+    // the class we're adding
+    $landscape_class = 'landscape';
+  	$portrait_class = 'portrait';
 
-        // if image is less than 480px, add their old classes back in plus our new class
-        if( $width > $height) {
-            $image->setAttribute('class', $portrait_class . $existing_classes);
-        }
-        else {
-            $image->setAttribute('class', $landscape_class . $existing_classes);
-        }
+    // if image is less than 480px, add their old classes back in plus our new class
+    if( $width > $height) {
+        $imageSrc->setAttribute('class', $portrait_class . $existing_classes);
     }
-
+    else {
+        $imageSrc->setAttribute('class', $landscape_class . $existing_classes);
+    }
+    
 }
 
 ?>
