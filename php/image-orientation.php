b2f9c7f713308f20a6aef1bab58d91b99564c53a
JHammond <ss_clan_cod@hotmail.com>
2013-04-07 15:32:09 +0100

    Added return stings for class names and added orientation classes to css

--- php/image-orientation.php
+++ php/image-orientation.php
@@ -1,27 +1,23 @@
 
 <?php 
 
-function getOrientation( $imageSrc ) {
+function getOrientation($imageSrc) {
 
 
-    // get the widths of each image
+    // get the width of image
     $width = $imageSrc->imagesx;
     $height = $imageSrc->imagesy;
-
-
-    // the existing classes already on the images
-    $existing_classes = $imageSrc->getAttribute('class');
 
     // the class we're adding
     $landscape_class = 'landscape';
   	$portrait_class = 'portrait';
 
-    // if image is less than 480px, add their old classes back in plus our new class
-    if( $width > $height) {
-        $imageSrc->setAttribute('class', $portrait_class . $existing_classes);
+    // if image is landscape or portrait add class name accordingly 
+    if( $imageSrc->$width > $imageSrc->$height) {
+        return $landscape_class;
     }
     else {
-        $imageSrc->setAttribute('class', $landscape_class . $existing_classes);
+        return $portrait_class;
     }
     
 }
